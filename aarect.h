54bc4ed07b367f64e58d56be100e3f6b1cddb498
YohnWang <347220312@qq.com>
2020-11-30 17:18:44 +0800

    fix bug at rect_t::is_in_rect()

--- aarect.h
+++ aarect.h
@@ -12,13 +12,13 @@ public:
     std::shared_ptr<material_t> mat_ptr;
     double d;
     rect_t()=default;
-    rect_t(vec3_t normal,point3_t a,point3_t b,std::shared_ptr<material_t> mat):n(normal),min(a),max(b),mat_ptr(mat)
+    rect_t(vec3_t normal,point3_t a,point3_t b,std::shared_ptr<material_t> mat):n(normal),mat_ptr(mat)
     {
-        //min=vec3_t(fmin(a.x,b.x),fmin(a.y,b.y),fmin(a.z,b.z));
-        //max=vec3_t(fmax(a.x,b.x),fmax(a.y,b.y),fmax(a.z,b.z));
+        min=vec3_t(fmin(a.x,b.x),fmin(a.y,b.y),fmin(a.z,b.z));
+        max=vec3_t(fmax(a.x,b.x),fmax(a.y,b.y),fmax(a.z,b.z));
         d=-dot(n,min);
     }
-    virtual std::pair<bool, hit_record_t> hit(const ray_t &r, double t_min, double t_max) const
+    virtual std::pair<bool, hit_record_t> hit(const ray_t &r, double t_min, double t_max) const override
     {
         auto denominator=dot(n,r.direction());
         if(denominator==0)
@@ -30,14 +30,13 @@ public:
         auto p=r.at(t);
         if(is_in_rect(p)==false)
             return {false,{}};
-        hit_record_t rec;
+        hit_record_t rec{};
         rec.p=p;
         rec.t=t;
         rec.mat_ptr=mat_ptr;
         rec.set_face_normal(r,n);
-        rec.u=(p.x-min.x)/(max.x-min.x);
-        rec.v=(p.y-min.y)/(max.y-min.y);
-        //fprintf(stderr,"%f %f\n",rec.u,rec.v);
+        rec.u=1;
+        rec.v=1;
         return {true,rec};
     }
     virtual std::pair<bool, aabb_t> bounding_box(double time0,double time1)const
@@ -48,9 +47,10 @@ public:
 
     bool is_in_rect(const point3_t &p) const
     {
-        auto x_in=p.x>=min.x && p.x<=max.x;
-        auto y_in=p.y>=min.y && p.y<=max.y;
-        auto z_in=p.z>=min.z && p.z<=max.z;
+        constexpr auto e=1e-8;
+        auto x_in=p.x>=min.x-e && p.x<=max.x+e;
+        auto y_in=p.y>=min.y-e && p.y<=max.y+e;
+        auto z_in=p.z>=min.z-e && p.z<=max.z+e;
         if( x_in && y_in && z_in )
             return true;
         else 
